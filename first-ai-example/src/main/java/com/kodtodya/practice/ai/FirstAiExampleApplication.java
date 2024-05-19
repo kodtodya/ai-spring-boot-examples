7b0f5348cdd5501bc04dab8ad09ceaa92a85f350
Avadhut <kodtodya@gmail.com>
2024-05-19 21:55:19 +0530

    initial import

---
+++ first-ai-example/src/main/java/com/kodtodya/practice/ai/FirstAiExampleApplication.java
@@ -0,0 +1,13 @@
+package com.kodtodya.practice.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstAiExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstAiExampleApplication.class, args);
+	}
+
+}
